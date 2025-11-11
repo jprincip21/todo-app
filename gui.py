804ed97ae3fd89f6cab17b44d7afe72b8fd398ac
jprincip21 <jprincip21@gmail.com>
2025-11-10 22:40:52 -0500

    Address Errors, Added Clock

--- gui.py
+++ gui.py
@@ -1,8 +1,12 @@
 import functions
 import FreeSimpleGUI as sg
+import time
+
+sg.theme("darkgrey8")
 
 # Creating items for GUI
 label = sg.Text("Type in a to-do")
+clock_label = sg.Text('', key="clock")
 
 input_box = sg.InputText(tooltip="Enter todo",
                          key="todo")
@@ -11,8 +15,8 @@ todos_box = sg.Listbox(values=functions.get_todos(),
                        enable_events=True,
                        size=(45,10))
 
-add_button = sg.Button("Add")
-edit_button = sg.Button("Edit")
+add_button = sg.Button("Add", size=8)
+edit_button = sg.Button("Edit", size=8)
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
@@ -22,20 +26,23 @@ add_complete_layout = [[edit_button],
 add_complete_column = sg.Column(add_complete_layout)
 
 #Create Layout List, Each list inside list is displayed as a row in the gui
-layout = [[label],
-          [input_box, add_button],
-          [todos_box, add_complete_column],
-          [exit_button]]
+layout = [[clock_label],
+        [label],
+        [input_box, add_button],
+        [todos_box, add_complete_column],
+        [exit_button]]
 
 window = sg.Window("My To-Do App",
                    layout=layout, #layout expects a list of lists (rows)
                    font=("Helvetica", 16))
 
 while True:
-    event, values = window.read() # Displays window
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200) # Displays window
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    window["clock"].update(value=time.strftime("%d-%m-%Y %H:%M:%S"))
     match event:
         # When the user clicks something the event variable is updated to be a string of the key.
         # We use this to decide what to do.
@@ -53,25 +60,32 @@ while True:
             # When the edit button is pressed we grab the todo the user clicked
             # by using the Listbox key in the values dictionary
             # We then get the todos list, find the index of the todo and update that index
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n"
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
 
-            window["todos"].update(values=todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                print("No Item selected")
+                sg.popup("Please select an item first", font=("Helvetica", 16))
 
         case "Complete":
-            selected_todo = values['todos'][0]
-            todos = functions.get_todos()
-            index = todos.index(selected_todo)
-            if selected_todo:
+            try:
+                selected_todo = values['todos'][0]
+                todos = functions.get_todos()
+                index = todos.index(selected_todo)
                 todos.remove(selected_todo)
                 functions.write_todos(todos)
                 window["todos"].update(values=todos)
                 window["todo"].update(value="")
+            except IndexError:
+                print("No Item selected")
+                sg.popup("Please select an item first", font=("Helvetica", 16))
 
         case "todos":
             # When the user clicks on a todo we grab the value from the values dictionary using the
@@ -83,8 +97,7 @@ while True:
         case "Exit":
             break
 
-        case sg.WIN_CLOSED:
-            break
+
 
 window.close()
 
